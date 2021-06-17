50539396a6471429ba982b750a8f6a3275aee1de
4zaran <karol.chanaj3@gmail.com>
2021-06-17 18:42:17 +0200

    Ready not working;

--- network/procedures/procedure_lobby_update.py
+++ network/procedures/procedure_lobby_update.py
@@ -21,7 +21,6 @@ def carry_out(sckt: socket.socket, frame: str) -> str:
         if not context.GAME.lobby.add_participant(jsonpickle.decode(body)):
             return utility.get_ip_and_address_of_client_socket(sckt) + " BAD UPDATE (no participant)"
 
-        context.GAME.view_manager.refresh()
     elif action == "PLAYER_LEFT":
         pid_text = utility.get_value_of_argument(frame, "ID")
         if pid_text == "":
@@ -33,12 +32,16 @@ def carry_out(sckt: socket.socket, frame: str) -> str:
         if not context.GAME.lobby.remove_participant(int(pid_text)):
             return utility.get_ip_and_address_of_client_socket(sckt) + " BAD UPDATE (could not remove player)"
 
-        context.GAME.view_manager.refresh()
     elif action == "LOBBY_CLOSE":
         context.GAME.abandon_lobby()
         context.GAME.view_manager.display_error_and_go_to("Lobby was closed.", Views.MENU)
+    elif action == "PLAYER_READY":
+        player_status = bool(utility.get_value_of_argument(frame, "STATUS"))
+        player_id = int(utility.get_value_of_argument(frame, "PLAYER_ID"))
+        context.GAME.lobby.get_participant_of_id(player_id).ready = player_status
     else:
         communicate(sckt, ["400"])
         return utility.get_ip_and_address_of_client_socket(sckt) + " BAD UPDATE (no procedure for action " + action + ")"
 
+    context.GAME.view_manager.refresh()
     return utility.get_ip_and_address_of_client_socket(sckt) + " LOBBY UPDATE"
