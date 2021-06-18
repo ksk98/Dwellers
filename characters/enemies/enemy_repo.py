c7e230a001f5c8c1cac95ba9e4d62a1a379b7eda
wiwur <krzysztofskrawczyk@gmail.com>
2021-06-18 11:17:18 +0200

    - 2 more enemies and repo added - balance changes to fire attack

---
+++ characters/enemies/enemy_repo.py
@@ -0,0 +1,36 @@
+import random
+
+from characters.enemies.enemy_base import EnemyBase
+from characters.enemies.enemy_blind_bloodbeast import BlindBloodbeast
+from characters.enemies.enemy_dweller import Dweller
+from characters.enemies.enemy_ghoul import Ghoul
+from characters.enemies.enemy_rouge_mage import RogueMage
+from characters.enemies.enemy_skeleton import Skeleton
+from characters.enemies.enemy_skeleton_hunter import SkeletonHunter
+from characters.enemies.enemy_skeleton_lazy import SkeletonLazy
+from characters.enemies.enemy_worshipper import Worshipper
+
+
+def roll_an_enemy() -> EnemyBase:
+    # Chances to roll enemy of a certain difficulty:
+    # Easy      20%
+    # Medium    45%
+    # Hard      30%
+    # Very Hard 5%
+    chance_roll = random.randint(0, 99)
+    if chance_roll in range(0, 20):
+        pool = [SkeletonLazy(), Dweller()]
+    elif chance_roll in range(20, 65):
+        pool = [Skeleton(), RogueMage()]
+    elif chance_roll in range(65, 95):
+        pool = [SkeletonHunter(), Worshipper(), Ghoul()]
+    else:
+        pool = [BlindBloodbeast()]
+
+    ind = random.randint(0, len(pool)-1)
+    print("ROLL: " + str(chance_roll))
+    return pool[ind]
+
+
+if __name__ == '__main__':
+    print(roll_an_enemy().name)
