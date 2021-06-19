7e51dbfe8cee0d65d8853efee7117e8dad716fd7
4zaran <karol.chanaj3@gmail.com>
2021-06-19 23:58:24 +0200

    BodyKoxXx removed; localhost removed

--- characters/saved_characters.py
+++ characters/saved_characters.py
@@ -1,11 +1,5 @@
 from characters.enums.stats_enum import Stat
 
 saved_characters = {
-    # Sample saved character
-    "BodyKoxXx": {
-        Stat.HEALTH: 50,
-        Stat.ENERGY: 20,
-        Stat.STRENGTH: 5,
-        "points": 10
-    }
+
 }
